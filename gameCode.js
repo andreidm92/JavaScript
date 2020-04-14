36cc22d2f2b46aaf2fd3331a82607bc5444bb7bc
andreidm92 <admi70@rambler.ru>
2020-04-14 15:16:03 +0300

    Домашняя работа 4

---
+++ gameCode.js
@@ -0,0 +1,141 @@
+var event, ok, n, m;
+var count = [];
+var question = [];
+
+do {//Выводим первый вопрос
+    ok = false;
+    event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
+    if (event == -1) {
+        break;
+    }
+    else {
+		m = "works.a" + event
+		ok = isAnswer(works.a00, event, m);
+    }
+} while (!ok);
+switch (event) {
+    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
+        do {
+            ok = false;
+            event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+				m = "works.b" + event
+                ok = isAnswer(works.b00, event, m);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+						m = "works.d" + event
+                        ok = isAnswer(works.d00, event, m);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+						m = "works.d" + event
+                        ok = isAnswer(works.d00, event, m);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        do {
+            ok = false;
+            event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+				m = "works.c" + event
+                ok = isAnswer(works.c00, event, m);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+						m = "works.d" + event
+                        ok = isAnswer(works.d00, event, m);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+						m = "works.d" + event
+                        ok = isAnswer(works.d00, event, m);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert('Ошибка');
+}
+alert('Спасибо за игру');
+for (var i = 1; i <= 3; i++){
+	n = +prompt('Введите номер окна от 1 до 3') - 1;
+	alert (question[n] +"\n" + "Ваш ответ :" + count[n]);
+
+		  }
+
+
+
+
+//------------------------------------------
+function isAnswer(q, event, m) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    }
+    else if (event < 1 || event > 2) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+	count.push(m);
+	question.push(q)
+	return true;
+    
+}
+
